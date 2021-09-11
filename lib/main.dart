a7372061ccd293db96ee2ca73b3b4223308125e1
MD TOSIF <mdtosif0mt@gmail.com>
2021-09-11 08:45:24 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:videotream/startServer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MyApp());
 }
 
 pickingFile() async {
+  await Permission.location.request();
+  // await Permission.locationAlways.request();
   FilePickerResult? res = await FilePicker.platform.pickFiles(
     type: FileType.video,
   );
@@ -35,6 +39,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -46,32 +51,48 @@ class _MyAppState extends State<MyApp> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              Padding(
+                padding: const EdgeInsets.all(26.0),
+                child: Text(
+                  'Select a video to host that video in your local network',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ),
               ElevatedButton(
                 onPressed: () {
                   Clipboard.setData(
                     ClipboardData(text: videoAddress),
                   );
                 },
-                child: Text('address = $videoAddress click to copy'),
+                child: videoAddress == null
+                    ? Text('Error')
+                    : Text('address = $videoAddress click to copy url'),
               ),
               Container(
                 alignment: Alignment.center,
                 child: ElevatedButton(
-                    onPressed: () {
-                      pickingFile().then((e) {
-                        videoPath = e;
-                        return startServer(videoPath);
-                      }).then((e) {
-                        setState(() {
-                          videoAddress = e;
-                        });
-                      }).catchError((e) {
-                        setState(() {
-                          videoAddress = "error";
-                        });
+                  onPressed: () {
+                    pickingFile().then((e) {
+                      videoPath = e;
+                      return startServer(videoPath);
+                    }).then((e) {
+                      setState(() {
+                        videoAddress = e;
                       });
-                    },
-                    child: Text('pick a file')),
+                    }).catchError((e) {
+                      setState(() {
+                        videoAddress = null;
+                      });
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'pick a file',
+                      style: Theme.of(context).accentTextTheme.headline6,
+                    ),
+                  ),
+                ),
               ),
             ]),
       ),
